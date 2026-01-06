a7d395ea12a87bd2853999ce7f21049ec1a64edf
KSY173 <doggysel@naver.com>
2026-01-06 00:36:28 -0500

    your message

--- pointer.c
+++ pointer.c
@@ -27,22 +27,43 @@ void swap(int num[])
 }
 */
 
- int main(void)
- {
-    int ary[5] = {10, 20, 30, 40, 50};
-    int *pa = ary;
-    int *pb = pa + 3;
 
-    printf("pa : %u\n", pa);
-    printf("pb : %u\n", pb);
-    pa++;
-    printf("pb - pa : %u\n", pb - pa);
-    printf("pa : %u\n", pa);
 
-    printf("앞에 있는 배열 요소의 값 출력: ");
-    if (pa < pb) printf("%d\n", *pa);
-    else printf("%d\n", *pb);
+
+
+/*
+int main(void)
+{
+    int a = 10;
+    int *p = &a;
+    double *pd;
+
+    pd = p;
+    printf("%lf\n", *pd);
+    //printf("%d\n", *(int *)pd);
 
     return 0;
- }
+}
+*/
+
+
+
+
+
+
+int main(void)
+{
+   int ary[5] = {10, 20, 30, 40, 50};
+   int *pa = ary;
+   int *pb = pa + 3;
+   printf("pa : %u\n", pa);
+   printf("pb : %u\n", pb);
+   pa++;
+   printf("pb - pa : %u\n", pb - pa);
+   printf("pa : %u\n", pa);
+   printf("앞에 있는 배열 요소의 값 출력: ");
+   if (pa < pb) printf("%d\n", *pa);
+   else printf("%d\n", *pb);
+   return 0;
+}
 
--- practice3.c
+++ practice3.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
-void line_detect();
+void line_detect(int value[], int label[], int N, int threshold);
+
+int count(int label[], int black_count, int N);
+
+double average(int line, int label[], int N);
 
 int main(void)
 {
-    int N, i;
+    int N, i, j;
     int threshold;
 
     printf("센서 개수 입력(0~16): ");
     scanf("%d", &N);
 
     int value[N];
+    int label[N];
 
     for(i=0; i<N; i++)
     {
@@ -22,23 +27,82 @@ int main(void)
     printf("threshold = ");
     scanf("%d", &threshold);
 
-    //line 판별 함수
-    void line_detect(int value[i])
+    line_detect(value, label, N, threshold);
+
+    int black_count = 0;
+
+    int line;
+    double index_average;
+
+    line = count(label, black_count, N);
+    index_average = average(line, label, N);
+
+    printf("label = ");
+    for(j = 0; j < N; j++)
     {
-        int i;
+        printf("%3d", label[j]);
+    }
+    printf("\n");
 
-        for(i=0; i<N; i++)
+    printf("black_count = %d\n", line);
+    printf("index_avg = %.2lf\n", index_average);
+
+    return 0;
+}
+
+//line 판별 함수
+void line_detect(int value[], int label[], int N, int threshold)
+{
+    int i;
+    for(i=0; i<N; i++)
+    {
+        if (value[i]<threshold)
         {
-            if (value[i]<threshold)
-            {
-                label[i] = 1;
-            }
-            else if ()
+            label[i] = 1;
         }
+        else if (value[i]>=threshold)
+        {
+            label[i] = 0;
+        }
+    }
+}
 
+//black_count 개수 세는 함수
+int count(int label[], int black_count, int N)
+{
+    int j;
+
+    for(j=0; j < N; j++)
+    {
+        if(label[j] == 1)
+        {
+            black_count++;
+        }
     }
 
-    //black_count 개수 세는 함수
+    return black_count;
+}
 
-    //index_avg 계산 함수
+//index_avg 계산 함수
+double average(int line, int label[], int N)
+{
+    int sum_index = 0;
+    double index_avg = 0;
+    int k;
+
+    for(k=0; k<N; k++)
+    {
+        sum_index += k*label[k];
+    }
+
+    if(line > 0)
+    {
+        index_avg = sum_index/line;
+    }
+    else if(line == 0)
+    {
+        printf("라인 없음\n");
+    }
+
+    return index_avg;
 }
