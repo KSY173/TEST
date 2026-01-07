d20c21b0884bbf3f13862f69e82b4c1eaa5168e0
KSY173 <doggysel@naver.com>
2026-01-07 02:51:01 -0500

    add morepointer

--- morepointer.c
+++ morepointer.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
+void swap_ptr(char **ppa, char **ppb);
+
 int main(void)
 {
+    char *pa = "success";
+    char *pb = "failure";
+
+    printf("pa -> %s, pb -> %s\n", pa, pb);
+    swap_ptr(&pa, &pb);                     //ppa에 pa의 주소값, ppb에 pb의 주소값을 받은 것
+    printf("pa -> %s, pb -> %s\n", pa, pb);
 
     return 0;
+}
+
+void swap_ptr(char **ppa, char **ppb)
+{
+    char *pt;
+
+    pt = *ppa;
+    *ppa = *ppb;
+    *ppb = pt;
 }
